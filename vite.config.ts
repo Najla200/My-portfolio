20fb7dc9c2720aa93a3a965979239b469eda35d4
Najla200 <najlahamza52@gmail.com>
2025-09-08 16:13:50 +0530

    Updated

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/my-portfolio/', // <--- Added for GitHub Pages
+  base: '/My-portfolio/', // <--- Added for GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
